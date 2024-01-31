e74da445c4ffc30053a68403fe7f2c35030b4739
2lag <aidenhelfman@gmail.com>
2024-01-31 15:56:44 -0600

    ye

--- js/blocker.js
+++ js/blocker.js
@@ -8,10 +8,11 @@ document.addEventListener( 'contextmenu',
 
 $( function() {
   $( document ).keydown( function( objEvent ) {
-    if( objEvent.ctrlKey && !enableKeys ) {
+    if( objEvent.ctrlKey ) {
       if( objEvent.shiftKey ) {
-        if( objEvent.keyCode == 73 || objEvent.keyCode == 74 || objEvent.keyCode == 67 )
-          return false; // CTRL + SHIFT + I / J / C
+        if( objEvent.keyCode == 73 || objEvent.keyCode == 74 ||
+            objEvent.keyCode == 75 || objEvent.keyCode == 67 )
+          return false; // CTRL + SHIFT + I / J / K / C
       } else {
         if( objEvent.keyCode == 65 || objEvent.keyCode == 85 )
           return false; // CTRL + A / U
