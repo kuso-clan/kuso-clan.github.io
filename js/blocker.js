d77a744c470f980969879405a4f87eb4e0d5f019
2lag <aidenhelfman@gmail.com>
2024-01-31 14:53:19 -0600

    blocker

---
+++ js/blocker.js
@@ -0,0 +1,23 @@
+// if you wanna see the source
+// https://github.com/kuso-clan/kuso-clan.github.io
+// not making my site ugly...
+
+document.addEventListener( 'contextmenu',
+  event => event.preventDefault()
+);
+
+$( function() {
+  $( document ).keydown( function( objEvent ) {
+    if( objEvent.ctrlKey && !enableKeys ) {
+      if( objEvent.shiftKey ) {
+        if( objEvent.keyCode == 73 || objEvent.keyCode == 74 || objEvent.keyCode == 67 )
+          return false; // CTRL + SHIFT + I / J / C
+      } else {
+        if( objEvent.keyCode == 65 || objEvent.keyCode == 85 )
+          return false; // CTRL + A / U
+      }
+    }
+    if( objEvent.keyCode == 123 )
+		  return false;// F12
+  });
+});
