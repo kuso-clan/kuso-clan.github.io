a8012c5268cac13b629852d895daad67e63b4282
2lag <aidenhelfman@gmail.com>
2024-05-09 15:34:51 -0500

    wow

--- js/blocker.js
+++ js/blocker.js
@@ -1,6 +1,4 @@
-// if you wanna see the source
-// https://github.com/kuso-clan/kuso-clan.github.io
-// not making my site ugly...
+// if you wanna see the source just go https://github.com/kuso-clan/kuso-clan.github.io
 
 document.addEventListener( 'contextmenu',
   event => event.preventDefault()
@@ -11,7 +9,8 @@ $( function() {
     if( objEvent.ctrlKey ) {
       if( objEvent.shiftKey ) {
         if( objEvent.keyCode == 73 || objEvent.keyCode == 74 ||
-            objEvent.keyCode == 75 || objEvent.keyCode == 67 )
+            objEvent.keyCode == 75 || objEvent.keyCode == 67 ||
+            objEvent.keyCode == 69 )
           return false; // CTRL + SHIFT + I / J / K / C
       } else {
         if( objEvent.keyCode == 65 || objEvent.keyCode == 85 )
